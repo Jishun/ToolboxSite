ba17301da894985f9fc902019184884c8f9ca05d
Jishun.Duan <Jishun.Duan@hotmail.com>
2016-06-16 16:15:08 +1000

    fix case sensitive issue of readme link

--- project/app/actions/home.js
+++ project/app/actions/home.js
@@ -4,6 +4,6 @@ import {error, info, success} from './common'
 export const RECEIVED_README = 'RECEIVED_README'
 
 export function getReadMeData(){
-  let url = 'https://raw.githubusercontent.com/djsxp/ToolboxSite/master/README.md'
+  let url = 'https://raw.githubusercontent.com/djsxp/ToolboxSite/master/readme.md'
   return http.get(url, RECEIVED_README, false)
 }
